4e3db7b29432d89d49747d9b4ee1cbf2af661864
Таня Бура <tanya.bura.99@gmail.com>
2022-01-28 16:44:42 +0200

    adding cart and possibility add new product. Updating application with memoized

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({currentUser }) =>{
+const Header = ({currentUser, hidden }) =>{
     return(
         <div className="header">
                 <Link className="logo-container" to='/'>
@@ -28,9 +31,19 @@ const Header = ({currentUser }) =>{
                 </Link>
                 )
             }
+            <CartIcon />
             </div>
+            {
+                hidden ? null : <CartDropdown />
+            }
         </div>
     )
 }
+const mapStateToProps =({user: { currentUser}, cart: {hidden}}) =>{
+    return {
+        currentUser,
+        hidden
+    }
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
