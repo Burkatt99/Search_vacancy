c170f546788843fe74997bdfba9bc4e7871d7703
Таня Бура <tanya.bura.99@gmail.com>
2022-01-30 14:28:43 +0200

    removing redux-logger from production bild

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import collectionsOverviewComponent from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
 const ShopPage = ({match}) =>(
-    <div className='shop-page'>
+    <div className='shop-page'> 
+        <Route exact path={`${match.path}`} component={collectionsOverviewComponent} />
      <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
         </div>
 );
